e37afd8e47b20bca5252460693ac806274c2fa53
MrChami119 <mrchamika119@gmail.com>
2025-04-20 22:29:58 +0530

    Update product.model.js

--- product-store/backend/models/product.model.js
+++ product-store/backend/models/product.model.js
@@ -20,3 +20,6 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema);
 
 export default Product;
+
+//MONGO_URI=mongodb+srv://atlas-sample-dataset-load-6804f4bc4495e5761441f1be:IZ6PQIAZFOWtgPNk@cluster0.1zjvwga.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0
+//IZ6PQIAZFOWtgPNk
