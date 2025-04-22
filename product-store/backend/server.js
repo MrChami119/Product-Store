94e042b4b3d41e9f505331610ade9a44357bcc1b
MrChami119 <mrchamika119@gmail.com>
2025-04-22 11:40:09 +0530

    Update server.js

--- product-store/backend/server.js
+++ product-store/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -26,6 +27,43 @@ app.post("/api/products", async (req,res) => {
         res.status(500).json({success:false, message:"Server Error"});
     }
 });
+
+app.delete("/api/products/:id", async (req,res) => {
+    const {id} = req.params;
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product deleted successfully"});
+    }catch(error){
+        res.status(404).json({success:false, message:"Product not found"});
+    }   
+});
+
+app.get("/api/products", async (req,res) => {
+    try{
+        const products = await Product.find();
+        res.status(200).json({success:true, data:products});
+    }catch(error){
+        console.error("Error in Get products:",error.message);
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+});
+
+app.put("/api/products/:id", async (req,res) => {
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid product ID"});
+    }
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data:updatedProduct});
+    }catch(error){
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+});
+
 
 app.listen(5000, () => {
     connectDB();
