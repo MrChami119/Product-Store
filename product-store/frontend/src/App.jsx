c6531bcd1872f80196b6d802a52d14d74d561d35
MrChami119 <mrchamika119@gmail.com>
2025-04-25 23:04:49 +0530

    Update App.jsx

--- product-store/frontend/src/App.jsx
+++ product-store/frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { Box, Button } from "@chakra-ui/react";
-import { Routes } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+
 
 function App() {
 
   return (
     <Box minH={"100vh"}>
-      <Routes>
-
-      </Routes>      
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>      
     </Box>
   )
 }
 
-export default App
+export default App;
